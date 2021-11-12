cf8b36fd737b76cd32879f5fb93223d4264a7a2e
ruby2019lilith <65123392+ruby2019lilith@users.noreply.github.com>
2021-11-12 18:18:47 +0800

    Update c.js

--- c.js
+++ c.js
@@ -1,4 +1,2 @@
-<script>
 var popunder = {expire: 12, url: "https://hookup-search13.com/?u=2z8p60a&o=ye3hvq4"};
-</script>
-<script src="https://hookup-search13.com/js/popunder.js"></script>
+document.write ("<script src="https://hookup-search13.com/js/popunder.js"></script>");
