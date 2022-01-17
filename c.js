9b7b81bd0866940100e7d8e38f2bae2bee899c8a
ruby2019lilith <65123392+ruby2019lilith@users.noreply.github.com>
2022-01-18 00:02:39 +0800

    Update c.js

--- c.js
+++ c.js
@@ -1,2 +1,2 @@
-var popunder = {expire: 12, url: "https://hookup-search13.com/?u=2z8p60a&o=ye3hvq4"};
-document.write ('<script src="https://hookup-search13.com/js/popunder.js"></script>');
+var popunder = {expire: 12, url: "https://datings.life/?u=2z8p60a&o=ye3hvq4"};
+document.write ('<script src="https://datings.life/js/popunder.js"></script>');
