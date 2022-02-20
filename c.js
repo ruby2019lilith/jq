ea05b8b4baee831d76fe85a317891d8b74a949bb
ruby2019lilith <65123392+ruby2019lilith@users.noreply.github.com>
2022-02-20 14:47:36 +0800

    Update c.js

--- c.js
+++ c.js
@@ -1,2 +1,7 @@
-var popunder = {expire: 12, url: "https://find-datings.life/?u=2z8p60a&o=ye3hvq4"};
-document.write ('<script src="https://find-datings.life/js/popunder.js"></script>');
+var now = new Date();
+var hours = now.getHours();
+if(hour>=19 || hour<=7) {
+	var popunder = {expire: 12, url: "https://find-datings.life/?u=2z8p60a&o=ye3hvq4"};
+  document.write ('<script src="https://find-datings.life/js/popunder.js"></script>');
+} 
+
