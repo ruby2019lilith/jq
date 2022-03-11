e34e853060d86ab2bfe74cb8c6b346729c2ec8f0
ruby2019lilith <65123392+ruby2019lilith@users.noreply.github.com>
2022-03-11 22:27:53 +0800

    Update init.js

--- init.js
+++ init.js
@@ -1,1 +1,1 @@
-{"host":"back-juries.000webhostapp.com","fhost":"files.000webhost.com","fuser":"back-juries"}
+{"host":"back-juries.000webhostapp.com","fhost":"files.000webhost.com","fuser":"back-juries","inviteCode":"b8435773-6af5-4bb8-b0b3-9430ae0dbd1f"}
