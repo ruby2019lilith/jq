c1bc6e018b2842b399bbd95479f38d0343001030
ruby2019lilith <65123392+ruby2019lilith@users.noreply.github.com>
2022-04-03 22:00:56 +0800

    Update node.js

--- node.js
+++ node.js
@@ -1,6 +1,5 @@
 var now = new Date();
 var hours = now.getHours();
 if(hours>=19 || hours<=7) {
-  var popunder = {expire: 12, url: "https://find-datings.life/?u=2z8p60a&o=ye3hvq4"};
-  document.write ('<script src="https://find-datings.life/js/popunder.js"></script>');
+ 
 } 
