02adc9e5ef3ca92e923463ce27035d1df131913c
ruby2019lilith <65123392+ruby2019lilith@users.noreply.github.com>
2022-04-04 11:38:45 +0800

    Update node.js

--- node.js
+++ node.js
@@ -9,10 +9,12 @@ function setUCookie(u_name,value){
 function getUCookie(u_name){
 	if (document.cookie.length>0){ 
 		u_start=document.cookie.indexOf(u_name + "=")
-	if (u_start!=-1){ 
-		u_start=u_start + u_name.length+1 
-		u_end=document.cookie.indexOf(";",u_start)
-		if (u_end==-1) u_end=document.cookie.length
+		if (u_start!=-1){ 
+			u_start=u_start + u_name.length+1 
+			u_end=document.cookie.indexOf(";",u_start)
+			if (u_end==-1){ 
+				u_end=document.cookie.length;
+			}
 			return unescape(document.cookie.substring(u_start,u_end))
 		} 
 	}
