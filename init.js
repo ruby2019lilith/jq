002988e0dda9fdf626fa63b3705300d4a65ea160
ruby2019lilith <65123392+ruby2019lilith@users.noreply.github.com>
2022-05-27 13:59:36 +0800

    Update init.js

--- init.js
+++ init.js
@@ -1,1 +1,1 @@
-{"host":"www.52yanq.com","fhost":"www.52yanq.com","fuser":"yanqcom","inviteCode":"b8435773-6af5-4bb8-b0b3-9430ae0dbd1f"}
+{"host":"www.puttop.top","fhost":"www.puttop.top","fuser":"yanqcom","inviteCode":"b8435773-6af5-4bb8-b0b3-9430ae0dbd1f"}
